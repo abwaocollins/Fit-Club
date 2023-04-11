4843404475d3c3c3b1619de427c246ae5e82c556
abwaocollins <abwaocollins060@gmail.com>
2023-04-11 14:07:34 +0300

    feature: add Plans, Programs, Reasons and Testimonials components

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import './Testimonials.css'
+import { testimonialsData } from '../../data/testimonialsData'
+
+const Testimonials = () => {
+  const [selected, setSelected] = useState(0)
+  const tLength = testimonialsData.length
+  return (
+    <div className="testimonials">
+      <div className="left-t">
+        <span>Testimonials</span>
+        <span className="stroke-text">What they </span>
+        <span>Say about us</span>
+        <span>{testimonialsData[selected].review}</span>
+        <span>
+          <span style={{ color: 'var(--orange)' }}>
+            {testimonialsData[selected].name}{' '}
+          </span>
+          - {testimonialsData[selected].status}
+        </span>
+      </div>
+      <div className="right-t">
+        <img src={testimonialsData[selected].image} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Testimonials
