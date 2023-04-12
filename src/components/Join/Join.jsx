7fd1b7af9ae40282c601973b14ec6bc6d0ae2811
abwaocollins <abwaocollins060@gmail.com>
2023-04-12 14:27:42 +0300

    general cleanup and work on footer and join us section

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Join.css'
+import emailjs from '@emailjs/browser'
 
 const Join = () => {
+  const form = useRef()
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_x64t0iv',
+        'template_ktknjj9',
+        form.current,
+        'd8xb_N-K1pELnM4JM',
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+        },
+        (error) => {
+          console.log(error.text)
+        },
+      )
+  }
   return (
     <div className="join" id="join-us">
       <div className="left-j">
+        <hr />
         <div>
           <span className="stroke-text">READY TO</span>
-          <span> LEVEL UP</span>
+          <span>LEVEL UP</span>
         </div>
         <div>
           <span>YOUR BODY</span>
-          <span className="stroke-text"> WITH US</span>
+          <span className="stroke-text">WITH US</span>
         </div>
       </div>
-      <div className="right-j"></div>
+      <div className="right-j">
+        <form ref={form} className="email-container" onSubmit={sendEmail}>
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Enter your email address"
+            id=""
+          />
+          <button className="btn btn-j">Join now</button>
+        </form>
+      </div>
     </div>
   )
 }
